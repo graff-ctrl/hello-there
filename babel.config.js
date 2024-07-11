ab05b0d0e08429a2debbaffa9c0b4e1f6c7e203f
GRAFF <agraff@98point6.com>
2024-07-11 00:20:50 -0700

    Fixing testing configuration and conflict with dotenv presets in babel. Initial tests for component

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,17 @@ module.exports = function (api) {
   return {
     presets: [
       'babel-preset-expo',
-      "module:react-native-dotenv"
     ],
     plugins: [
-    ['module:react-native-dotenv']
-    ]
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
+   ]
   };
 
 };
