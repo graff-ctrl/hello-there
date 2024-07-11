1f8f755842ad87305c4310600c6793819496ce2d
GRAFF <agraff@98point6.com>
2024-07-11 11:14:50 -0700

    removing .env

--- ctx.tsx
+++ ctx.tsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import { useStorageState } from './src/expo-auth/useStorage';
+import React from "react"
+import { useStorageState } from "./src/expo-auth/useStorage"
 
 const AuthContext = React.createContext<{
-  signIn: () => void;
-  signOut: () => void;
-  session?: string | null;
-  sessionLoading: boolean;
+  signIn: () => void
+  signOut: () => void
+  session?: string | null
+  sessionLoading: boolean
 }>({
   signIn: () => null,
   signOut: () => null,
   session: null,
   sessionLoading: false,
-});
+})
 
 // This hook can be used to access the user info.
 export function useSession() {
-  const value = React.useContext(AuthContext);
-  if (process.env.NODE_ENV !== 'production') {
+  const value = React.useContext(AuthContext)
+  if (process.env.NODE_ENV !== "production") {
     if (!value) {
-      throw new Error('useSession must be wrapped in a <SessionProvider />');
+      throw new Error("useSession must be wrapped in a <SessionProvider />")
     }
   }
 
-  return value;
+  return value
 }
 
 export function SessionProvider(props: React.PropsWithChildren) {
-  const [[sessionLoading, session], setSession] = useStorageState('session');
+  const [[sessionLoading, session], setSession] = useStorageState("session")
 
   return (
     <AuthContext.Provider
       value={{
         signIn: () => {
-          console.log('User is attempting to sign in')
-          setSession('xxx');
+          console.log("User is attempting to sign in")
+          setSession("xxx")
         },
         signOut: () => {
-          console.log('User is attempting to sign out. ')
-          setSession(null);
+          console.log("User is attempting to sign out. ")
+          setSession(null)
         },
         session,
         sessionLoading,
-      }}>
+      }}
+    >
       {props.children}
     </AuthContext.Provider>
-  );
+  )
 }
